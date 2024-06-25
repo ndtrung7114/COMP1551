2f52bd702f40cdf257462d1862e29971ef5024d0
ndtrung7114 <trungnd7114@gmail.com>
2024-06-25 11:40:02 +0700

    new file

---
+++ WindowsFormsApp3/Form_login.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form_login : Form
+    {
+        public Form_login()
+        {
+            InitializeComponent();
+            tb_password.PasswordChar = '*';
+        /* try
+            {
+                StreamReader reader = new StreamReader("login.txt");
+                string line = reader.ReadLine();
+                if (line != null)
+                {
+                    string[] infor = line.Split(',');
+                    tb_user.Text = infor[0];
+                    tb_password.Text = infor[1];
+                }
+                reader.Close();
+
+            } catch
+            {
+                MessageBox.Show("can not find login.txt");
+            }*/
+            
+            
+        }
+
+        private bool check_login()
+        {
+            try
+            {
+                StreamReader reader = new StreamReader("login1.txt");
+                string line = reader.ReadLine();
+                while(line != null)
+                {
+                        
+                    string text = tb_user.Text + "," + tb_password.Text + "," + cb_role.Text;
+                    if (line == text)
+                    {
+                        reader.Close();
+                        return true;
+                        
+                        
+                    }
+                    line = reader.ReadLine();
+                }
+
+            }
+            catch
+            {
+
+            }
+            return false;
+        }
+
+
+
+        
+
+        
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            if(check_login())
+            {
+                if (this.checkBox1.Checked)
+                {
+                    StreamWriter write = new StreamWriter("login1.txt", true);
+                    write.WriteLine("{0},{1},{2}", tb_user.Text, tb_password.Text, cb_role.Text);
+                    write.Close();
+                    MessageBox.Show("You have remeber this account");
+                }
+                MessageBox.Show("login successfull");
+                Form_admin form_Admin = new Form_admin();
+                form_Admin.ShowDialog();
+            } else
+            {
+                MessageBox.Show("login fail");
+            }
+            
+
+        }
+
+       
+    }
+}
---
+++ winformapp1/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winformapp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello");
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string temp = this.textBox2.Text;
+             
+            this.textBox2.Text = this.textBox1.Text;
+            this.textBox1.Text = temp;
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt_equal_Click(object sender, EventArgs e)
+        {
+            int first_number = Convert.ToInt32(this.firt_number.Text);
+            int second_number = Convert.ToInt32(this.second_number.Text);
+
+            if (cb.Text == "+")
+            {
+                int result = first_number + second_number;
+                this.tex_result.Text = result.ToString();
+            } else if ( cb.Text == "-" )
+            {
+                int result = first_number - second_number;
+                this.tex_result.Text = result.ToString();
+            } else if ( cb.Text == "*" )
+            {
+                int result = first_number * second_number;
+                this.tex_result.Text = result.ToString();
+            } else
+            {
+                int result = first_number / second_number;
+                this.tex_result.Text = result.ToString();
+            }
+
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string first_number, second_number;
+            string result;
+
+            StreamWriter textwriter = new StreamWriter("myfile.txt");
+            first_number = Convert.ToString(this.firt_number.Text);
+            second_number = Convert.ToString(this.second_number.Text);
+           
+            result = Convert.ToString(this.tex_result);
+            textwriter.Write("{0} {1} {2} = {3}",first_number, this.cb.Text, second_number, result);
+            textwriter.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            StreamReader textreader = new StreamReader("myfile.txt");
+            richTextBox1.Text = textreader.ReadToEnd();
+        }
+    }
+}
